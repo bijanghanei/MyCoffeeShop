ac54765efa36297fa994fe7c959a7834e35321a3
bijanghanei <iambijanghanei@gmail.com>
2024-02-11 23:47:24 +0330

    orderservice interface implemented

--- MyCoffeeShop/MyCoffeeShop.Core/Contracts/IOrderService.cs
+++ MyCoffeeShop/MyCoffeeShop.Core/Contracts/IOrderService.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using MyCoffeeShop.Core.dto;
+using MyCoffeeShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace MyCoffeeShop.Core.Contracts
 {
     public interface IOrderService
     {
+        void CreatOrderItems(Order order, List<CartItemDto> cartItems);
+        void UpdateOrder(Order order);
+        List<OrderItem> GetAllOrders();
+        Order GetOrder(string orderId);
     }
 }
